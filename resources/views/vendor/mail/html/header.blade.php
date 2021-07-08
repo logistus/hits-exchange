9f107d2311a80eef5c1b76f267a71e978e4d83f6
Sinan <logistus@gmail.com>
2021-07-09 01:11:55 +0300

    email layout header update

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,7 @@
       @if (trim($slot) === 'Laravel')
       <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
       @else
-      <a href="{{ url('/') }}" class="logo">LOGI TE</a>
+      <a href="{{ url('/') }}"><img src="{{ asset('images/hits.exchange.svg') }}" style="width: 250px; height: 50px;" /></a>
       @endif
     </a>
   </td>
