6eee3ee9c0b11c3aa5ad2053fc2ea5f51168a831
Sinan <logistus@gmail.com>
2021-07-30 02:25:39 +0300

    Email Layout Update

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,7 @@
       @if (trim($slot) === 'Laravel')
       <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
       @else
-      <a href="{{ url('/') }}"><img src="http://cdn.mcauto-images-production.sendgrid.net/c4eb48fa249c9e3a/bfe90452-82c8-4ecd-8418-b9346550c7b1/91x90.png" alt="{{ config('app.name') }}" style="width: 91px; height: 90px;" /></a>
+      <img src="{{ asset('images/hits.exchange.logo.png') }}" alt="{{ config('app.name') }}" style="width: 91px; height: 90px;" />
       @endif
     </a>
   </td>
